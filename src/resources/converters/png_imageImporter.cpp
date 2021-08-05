e33ec311f935450b3909501600276c52db17e940
Tiaan Louw <tiaanl@gmail.com>
2021-08-05 09:22:52 +0200

    Use new code from silhouette.

--- src/resources/converters/png_imageImporter.cpp
+++ src/resources/converters/png_imageImporter.cpp
@@ -1,14 +1,17 @@
-#include "legion/Resources/Converters/png_imageImporter.h"
+#include "legion/resources/converters/png_imageImporter.h"
+
+#include "silhouette/codec/image/png.h"
 
 namespace le {
 
 bool PNGImageImporter::import(nu::StringView name, nu::InputStream* stream, si::Image* storage) {
-  bool result = storage->load_from_png(stream);
-
-  if (!result) {
+  auto maybe_image = si::load_image_from_png(stream);
+  if (!maybe_image.has_value()) {
     LOG(Warning) << "Could not load image. (" << name << ")";
     return false;
   }
+
+  *storage = std::move(*maybe_image);
 
   return true;
 }
